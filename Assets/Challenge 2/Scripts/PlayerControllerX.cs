843205c8499d80bf34cc8fc42c1c8b9a93f519ba
Huesitos <62262851+hi-im-huesitos@users.noreply.github.com>
2021-04-05 19:05:45 -0400

    Completed Challenge 2 - Play Fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timeBetweenShots = 0.3333f;
+    private float timeStamp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time >= timeStamp && (Input.GetKeyDown(KeyCode.Space)))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeStamp = Time.time + timeBetweenShots;
+        }
+    }
+}
